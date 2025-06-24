f896714092425a9c74c863876ffbf4ec7008bcf4
miguel medina <mmedina@agilisa.com>
2025-06-24 14:41:19 -0400

    add amount to refundbyid request

---
+++ agilpay-client/models/RefundByIdRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace agilpay.models
+{
+    public class RefundByIdRequest
+    {
+        public string MerchantKey { get; set; }
+        public string IDTransaction { get; set; }
+
+        public string ExtData { get; set; }
+
+        public decimal Amount { get; set; } = 0;
+    }
+}
